7e6f1c044c711b651480d70059465fd73d5ba535
Shiva Harris <shivaharris@gmail.com>
2013-11-08 11:04:59 -0800

    Init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bandslist::Application.config.secret_key_base = '5e83e5d5f0a057693fc69f0b775d1da6d65dc1063c7abbbd1b5698cbafce4517044d9b8049d88204a17c5d987e9e5eeaf52b7f89c3549640da96f4bc0e981fbf'
